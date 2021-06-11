a8ebdf7194724aec419592833c2994f9952bf6da
kimyou <kimyouby@hotmail.com>
2021-06-10 23:40:50 -0700

    last commit

--- App/weather/functions/search-weather.js
+++ App/weather/functions/search-weather.js
@@ -6,19 +6,19 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.weather_table;
 const resultNumber = process.env.RESULT_NUMBER || 8;
 
-const fetchWeatherByName = (name, number) => {
+const fetchWeatherByCity = (city, number) => {
   const req = {
     TableName: tableName,
     Limit: number,
-    FilterExpression: 'contains(names, :name)',
-    ExpressionAttributeValues: { ':name': name },
+    FilterExpression: 'contains(cities, :city)',
+    ExpressionAttributeValues: { ':city': city },
   };
   return dynamodb.scan(req).promise();
 };
 
 module.exports.handler = async (event, context, callback) => {
-  const { name } = JSON.parse(event.body);
-  const weather = await fetchWeatherByName(name, resultNumber);
+  const { city } = JSON.parse(event.body);
+  const weather = await etchWeatherByCity(city, resultNumber);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(weather),
--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # CS687-Capstone-Project
 
-##Source codes reference:
-##STC TA
-##stcta
-##School of Technology and Computing - City University of Seattle
+### A simple weather application with AWS cloud computing services.
+#### Technology:
+- HTML, CSS, JavaScript
+- Node.js
+- AWS Lambda
+- Amazon S3
+- Amazon API Gateway
+- Amazon DynamoDB
+## Source codes reference:
+STC TA<br/>
+School of Technology and Computing - City University of Seattle
